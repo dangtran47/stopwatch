9a76e2d9bf56b5bb0c3042e6fea246e730be6073
Dang Tran <dang.tran@employmenthero.com>
2022-04-14 08:28:56 +0700

    Fix: formatTime hour

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const formatTime = milliseconds => {
   const ms = Math.floor(milliseconds % 1000)
   const seconds = Math.floor(milliseconds / 1000) % 60
   const minutes = Math.floor(milliseconds / (60 * 1000)) % 60
-  const hours = Math.floor(milliseconds / (60 * 60 * 1000)) % 60
+  const hours = Math.floor(milliseconds / (60 * 60 * 1000)) % 24
 
   return `${padToNDigits(2)(hours)}:${padToNDigits(2)(minutes)}:${padToNDigits(2)(seconds)}.${padToNDigits(3)(ms)}`
 }
